bdd81b7b8d852f4ef6cb01f1b56a75d859638a13
Jeongeun Lee <zaekoch@gmail.com>
2025-02-03 23:57:17 +0900

    design: padding, bg-color 변경

--- src/components/header/DropdownMenu.tsx
+++ src/components/header/DropdownMenu.tsx
@@ -58,7 +58,7 @@ const DropdownMenu = ({ pathname }: { pathname: string }) => {
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -10 }}
               transition={{ duration: 0.2, ease: "easeInOut" }}
-              className="absolute top-full right-0 bg-[#f4f4f4] border rounded-xl -mt-4 overflow-hidden shadow-2xl z-50"
+              className="absolute top-full right-0 bg-[#F4F4F4] border rounded-xl -mt-4 overflow-hidden shadow-2xl z-50"
             >
               <div className="min-w-[200px] text-14">
                 <div>
@@ -69,7 +69,7 @@ const DropdownMenu = ({ pathname }: { pathname: string }) => {
                     {dropdownMenu.map((menu) => (
                       <li
                         key={menu.title}
-                        className="px-2 py-2 hover:bg-background-tag rounded"
+                        className="px-2 py-[6px] hover:bg-[#e7e7e7] rounded"
                       >
                         <Link
                           href={menu.link}
@@ -85,7 +85,7 @@ const DropdownMenu = ({ pathname }: { pathname: string }) => {
                   </ul>
                 </div>
                 <div className="px-3 py-2 border-b">
-                  <button className="w-full flex items-center gap-1 px-3 py-2 hover:bg-background-tag rounded">
+                  <button className="w-full flex items-center gap-1 px-3 py-[6px] hover:bg-[#e7e7e7] rounded">
                     <MdNightlight />
                     <span>다크모드</span>
                   </button>
@@ -98,7 +98,7 @@ const DropdownMenu = ({ pathname }: { pathname: string }) => {
                     {socialLinks.map((link, index) => (
                       <li
                         key={index}
-                        className="px-2 py-2 hover:bg-background-tag rounded group"
+                        className="px-2 py-[6px] hover:bg-[#e7e7e7] rounded group"
                       >
                         <Link
                           href={link.href}

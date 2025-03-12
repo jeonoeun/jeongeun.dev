2807b2db067947e7b1ec2e09f4299cea206c7070
Jeongeun Lee <zaekoch@gmail.com>
2025-03-12 11:50:16 +0900

    style: PageTitle margin 수정

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -6,7 +6,9 @@ interface PageTitleProps {
 export default function PageTitle({ title, description }: PageTitleProps) {
   return (
     <div>
-      <h1 className="font-bold text-[44px] mb-4">{title}</h1>
+      <h1 className={`font-bold text-[44px] ${description ? "mb-4" : ""}`}>
+        {title}
+      </h1>
       {description && <p className="text-[#A09F9C]">{description}</p>}
     </div>
   );
